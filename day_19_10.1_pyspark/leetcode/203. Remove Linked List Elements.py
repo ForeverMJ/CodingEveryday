00c5a030c3c4bbacdb76ae617cf66ae80f9c150e
ForeverMJ <mj199607@gmail.com>
2022-10-02 01:25:27 +0900

    day019 10.1 最近は忙しくて pyspark プロジェクトに取り組む時間がありませんが、毎日 leetcode アルゴリズムの問題をこなしています。

---
+++ day_19_10.1_pyspark/leetcode/203. Remove Linked List Elements.py
@@ -0,0 +1,26 @@
+'''
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+示例 1：
+
+输入：head = [1,2,6,3,4,5,6], val = 6
+输出：[1,2,3,4,5]
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy_head = ListNode(next=head) #添加一个虚拟节点
+        cur = dummy_head
+        while(cur.next!=None):
+            if(cur.next.val == val):
+                cur.next = cur.next.next #删除cur.next节点
+            else:
+                cur = cur.next
+        return dummy_head.next
