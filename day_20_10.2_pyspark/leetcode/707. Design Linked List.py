853294a094566b9ef029f186c32104a12b1d8588
ForeverMJ <mj199607@gmail.com>
2022-10-02 22:20:20 +0900

    day020 10.2 leetcode707 and pyspark  関連のクラスターをデプロイすると、多くの予期しない問題が発生し、多くの時間が費やされました。

---
+++ day_20_10.2_pyspark/leetcode/707. Design Linked List.py
@@ -0,0 +1,198 @@
+'''
+
+Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
+A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
+If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+
+Implement the MyLinkedList class:
+
+MyLinkedList() Initializes the MyLinkedList object.
+int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
+void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+void addAtTail(int val) Append a node of value val as the last element of the linked list.
+void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
+void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
+
+设计链表的实现。您可以选择使用单链表或双链表。单链表中的节点应该具有两个属性：val 和 next。val 是当前节点的值，next 是指向下一个节点的指针/引用。如果要使用双向链表，则还需要一个属性 prev 以指示链表中的上一个节点。假设链表中的所有节点都是 0-index 的。
+
+在链表类中实现这些功能：
+
+get(index)：获取链表中第index个节点的值。如果索引无效，则返回-1。
+addAtHead(val)：在链表的第一个元素之前添加一个值为val的节点。插入后，新节点将成为链表的第一个节点。
+addAtTail(val)：将值为val 的节点追加到链表的最后一个元素。
+addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val  的节点。如果 index 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
+deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
+
+示例：
+
+MyLinkedList linkedList = new MyLinkedList();
+linkedList.addAtHead(1);
+linkedList.addAtTail(3);
+linkedList.addAtIndex(1,2);   //链表变为1-> 2-> 3
+linkedList.get(1);            //返回2
+linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+linkedList.get(1);            //返回3
+
+
+'''
+
+
+
+
+# 单链表
+class Node:
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class MyLinkedList:
+
+    def __init__(self):
+        self._head = Node(0)  # 虚拟头部节点
+        self._count = 0  # 添加的节点数
+
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        if 0 <= index < self._count:
+            node = self._head
+            for _ in range(index + 1):
+                node = node.next
+            return node.val
+        else:
+            return -1
+
+    def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+        self.addAtIndex(0, val)
+
+    def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+        self.addAtIndex(self._count, val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
+        if index < 0:
+            index = 0
+        elif index > self._count:
+            return
+
+        # 计数累加
+        self._count += 1
+
+        add_node = Node(val)
+        prev_node, current_node = None, self._head
+        for _ in range(index + 1):
+            prev_node, current_node = current_node, current_node.next
+        else:
+            prev_node.next, add_node.next = add_node, current_node
+
+    def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        if 0 <= index < self._count:
+            # 计数-1
+            self._count -= 1
+            prev_node, current_node = None, self._head
+            for _ in range(index + 1):
+                prev_node, current_node = current_node, current_node.next
+            else:
+                prev_node.next, current_node.next = current_node.next, None
+
+
+# 双链表
+# 相对于单链表, Node新增了prev属性
+class Node:
+
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+
+class MyLinkedList:
+
+    def __init__(self):
+        self._head, self._tail = Node(0), Node(0)  # 虚拟节点
+        self._head.next, self._tail.prev = self._tail, self._head
+        self._count = 0  # 添加的节点数
+
+    def _get_node(self, index: int) -> Node:
+        # 当index小于_count//2时, 使用_head查找更快, 反之_tail更快
+        if index >= self._count // 2:
+            # 使用prev往前找
+            node = self._tail
+            for _ in range(self._count - index):
+                node = node.prev
+        else:
+            # 使用next往后找
+            node = self._head
+            for _ in range(index + 1):
+                node = node.next
+        return node
+
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        if 0 <= index < self._count:
+            node = self._get_node(index)
+            return node.val
+        else:
+            return -1
+
+    def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+        self._update(self._head, self._head.next, val)
+
+    def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+        self._update(self._tail.prev, self._tail, val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
+        if index < 0:
+            index = 0
+        elif index > self._count:
+            return
+        node = self._get_node(index)
+        self._update(node.prev, node, val)
+
+    def _update(self, prev: Node, next: Node, val: int) -> None:
+        """
+            更新节点
+            :param prev: 相对于更新的前一个节点
+            :param next: 相对于更新的后一个节点
+            :param val:  要添加的节点值
+        """
+        # 计数累加
+        self._count += 1
+        node = Node(val)
+        prev.next, next.prev = node, node
+        node.prev, node.next = prev, next
+
+    def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        if 0 <= index < self._count:
+            node = self._get_node(index)
+            # 计数-1
+            self._count -= 1
+            node.prev.next, node.next.prev = node.next, node.prev
